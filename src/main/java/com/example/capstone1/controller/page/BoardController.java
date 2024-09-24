ca9cdd1d8dd4daf1429f9cacc96de3315a71c4ba
abedcsj <abedcsj@gmail.com>
2024-09-24 19:53:42 +0900

    week5-1

--- src/main/java/com/example/capstone1/controller/page/BoardController.java
+++ src/main/java/com/example/capstone1/controller/page/BoardController.java
@@ -1,5 +1,6 @@
 package com.example.capstone1.controller.page;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoardController {
 
+
+    @GetMapping("board/create")
+    public String create() {
+        return "board/create";
+    }
 
     @GetMapping("/{page}")
     public String page(@PathVariable String page) {
